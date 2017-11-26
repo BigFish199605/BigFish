2eccf756facd999d3c513758393473dabec3846d
BigFish199605 <wang1445398480@163.com>
2017-11-26 09:48:46 +0800

    1126-3

---
+++ SnackCase/src/main/java/com/snackcase/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.snackcase.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OrderController {
+	@RequestMapping
+	public String toAdd(){
+		return "";
+	}
+}
---
+++ SnackCase/src/main/java/com/snackcase/pojo/Order.java
@@ -0,0 +1,9 @@
+package com.snackcase.pojo;
+
+public class Order {
+	private String orderId;//订单id
+	private String payment;//支付状态
+	private Integer paymentType;//支付类型
+	private String postFree;//邮费
+	private Integer status;//
+}
